cbfb4647ba58c5f3c78a806a8d331aef54e727e4
Simon_Wang <simon971234@gmail.com>
2024-06-12 18:46:40 +0800

    Delete Plot directory

--- Plot/plot_MRI.py
+++
@@ -1,9 +0,0 @@
-
-import matplotlib.pyplot as plt
-
-def plot_2d_slice(img):
-    plt.imshow(img, cmap='gray', origin='lower')
-    plt.xlabel('First axis')
-    plt.ylabel('Second axis')
-    plt.colorbar(label='Signal intensity')
-    plt.show()
