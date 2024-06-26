b00509b3b90e71873b11fdbbf3b801c00c5512af
Simon_Wang <simon971234@gmail.com>
2024-06-26 11:03:39 +0800

    Update train.py

--- Transfer-Learning/scripts/train.py
+++ Transfer-Learning/scripts/train.py
@@ -13,11 +13,11 @@ def train_epoch(args, model, train_dataloader, valid_dataloader, criterion, opti
     best_epoch = 0
     best_loss = float('inf')
     # Train the model
-    results_tr,results_val = [], []
+    results_val = []
     for epoch in range(args.num_epochs):
         print(f"Starting epoch {epoch+1}")
         # Run training epoch and compute training loss
-        tr_loss, tr_predicts,tr_truths = run_epoch(args, model, train_dataloader, criterion,optimizer,  lr_scheduler,  fold, epoch, is_training=True)
+        run_epoch(args, model, train_dataloader, criterion,optimizer,  lr_scheduler,  fold, epoch, is_training=True)
         # Run validation epoch and compute validation loss
         with torch.no_grad():
             val_loss, val_predicts,val_truths = run_epoch(args, model, valid_dataloader, criterion, None,  None, fold, epoch, is_training=False)
@@ -27,8 +27,6 @@ def train_epoch(args, model, train_dataloader, valid_dataloader, criterion, opti
             best_loss = val_loss
             counter = 0
             best_epoch = epoch
-            best_epoch_predicts_tr = tr_predicts
-            best_epoch_labels_tr = tr_truths
             best_epoch_predicts_val = val_predicts
             best_epoch_labels_val = val_truths
         else:
@@ -42,12 +40,6 @@ def train_epoch(args, model, train_dataloader, valid_dataloader, criterion, opti
         except Exception as e:
             print(f"Error occurred while saving model: {e}")
         # Store results for the fold
-        results_tr.append({
-                "fold": fold + 1,
-                "epoch": best_epoch + 1,
-                "predictions": best_epoch_predicts_tr,
-                "labels": best_epoch_labels_tr
-            })
         results_val.append({
                 "fold": fold + 1,
                 "epoch": best_epoch + 1,
@@ -64,4 +56,4 @@ def train_epoch(args, model, train_dataloader, valid_dataloader, criterion, opti
             except FileNotFoundError:
                 print(f"File not found, skipping: {model_file}")
     print(f"Best epoch for fold {fold + 1}: {best_epoch + 1}")
-    return results_tr, results_val
+    return results_val
