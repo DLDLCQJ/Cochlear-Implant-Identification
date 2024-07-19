4b81535cf8a189aacff172949277172e1dd95e63
Simon_Wang <simon971234@gmail.com>
2024-07-19 18:04:00 +0800

    Update test.py

--- Transfer-Learning/scripts/test.py
+++ Transfer-Learning/scripts/test.py
@@ -5,14 +5,12 @@ from scripts.run_epoch import run_epoch
 
 def test_epoch(args, model, test_dataloader, criterion, fold):
     results_te = []
-    for epoch in range(args.num_epochs):
-        print(f"Starting epoch {epoch+1}")
-        with torch.no_grad():
-            te_loss, te_predicts,te_truths = run_epoch(args, model, test_dataloader, criterion, None, None, fold, epoch, is_training=False,is_testing=True)
-        results_te.append({
-            "fold": fold + 1,
-            "epoch": epoch + 1,
-            "predictions": te_predicts,
-            "labels": te_truths
-        })    
+    with torch.no_grad():
+        te_loss, te_predicts,te_truths = run_epoch(args, model, test_dataloader, criterion, None, None, fold, epoch=0, is_training=False,is_testing=True)
+    results_te.append({
+        "fold": fold + 1,
+        "epoch": epoch + 1,
+        "predictions": te_predicts,
+        "labels": te_truths
+    })    
     return results_te
